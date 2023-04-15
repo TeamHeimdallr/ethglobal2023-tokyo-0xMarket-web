94f762be7472b040db58b40c04565328198428b2
Imchaemin <chaaaamni@gmail.com>
2023-04-15 10:02:25 +0900

    chore: remove unnecessary files

--- src/components/notification/notification-icon.tsx
+++
@@ -1,8 +0,0 @@
-import tw from 'twin.macro';
-
-import { ButtonIconLarge } from '../buttons';
-import { IconNoti } from '../icons';
-
-export const NotificationIcon = () => {
-  return <ButtonIconLarge icon={<IconNoti />} />;
-};
