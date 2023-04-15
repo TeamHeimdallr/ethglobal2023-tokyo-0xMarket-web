7dd11f92df255144898ade35479682eee92c9996
Imchaemin <chaaaamni@gmail.com>
2023-04-15 19:57:05 +0900

    lint fix

--- src/pages/detail-mock/components/account-info.tsx
+++ src/pages/detail-mock/components/account-info.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useMemo } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { InjectedConnector } from '@wagmi/core';
