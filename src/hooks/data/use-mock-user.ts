bad52e372f17cb0a590723ef89b3576c05945613
lucky7323 <eunho.ambition@gmail.com>
2023-04-15 16:55:15 +0900

    chore: add sample verified

--- src/hooks/data/use-mock-user.ts
+++ src/hooks/data/use-mock-user.ts
@@ -1,7 +1,8 @@
 import { useEffect, useMemo } from 'react';
+import { sub } from 'date-fns';
 import { useLocalStorage } from 'usehooks-ts';
 
-import { Account, CATEGORIES } from '~/types';
+import { Account, CATEGORIES, UMA_VERIFY_STATUS } from '~/types';
 
 import { accountDetail as accountDetail1 } from '~/__mocks__/data/account-detail-1';
 import { accountDetail as accountDetail2 } from '~/__mocks__/data/account-detail-2';
@@ -26,8 +27,26 @@ export const useMockUser = () => {
         price: 999,
         tokenValue: 999,
 
-        verified: [],
         hidden: false,
+        verified: [
+          {
+            id: '1',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            status: UMA_VERIFY_STATUS.SUCCESS,
+            date: new Date(),
+          },
+          {
+            id: '2',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            status: UMA_VERIFY_STATUS.PENDING,
+          },
+          {
+            id: '3',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            status: UMA_VERIFY_STATUS.FAIL,
+            date: sub(new Date(), { hours: 7 }),
+          },
+        ],
       },
     ],
     []
